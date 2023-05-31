a11d380eb946e1d4b4919e72e4220f152a9bb280
Jasper Nygaard <jasper@sovs.net>
2023-05-31 18:34:51 +0200

    Filter repos with scheduled listeners into seperate file (#65)
    * renamed repo_github_actions.sh to repo_actions.sh. Added jq filter to find repos with scheduled triggers
    
    * renamed repo_github_actions.sh to repo_actions.sh. Added jq filter to find repos with scheduled triggers
    
    * renamed repo_github_actions.sh to repo_actions.sh. Added jq filter to find repos with scheduled triggers
    
    * Update scripts/repo_actions.sh
    
    Co-authored-by: Stefan Stölzle <stoe@github.com>
    
    * Update scripts/repo_actions.sh
    
    Co-authored-by: Stefan Stölzle <stoe@github.com>
    
    * Update scripts/repo_actions.sh
    
    Co-authored-by: Stefan Stölzle <stoe@github.com>
    
    * added script for fetching last run for all workflows
    
    * added script for fetching last run for all workflows
    
    * added script for fetching last run for all workflows
    
    * added script for fetching last run for all workflows
    
    ---------
    
    Co-authored-by: Stefan Stölzle <stoe@github.com>

---
+++ scripts/repo_actions_last_run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -eo pipefail
+
+echo "[]" > actions_workflows_last_run.json
+
+REPOS=$(jq -r ".[].name" repos.json)
+
+while read -r repo ; do
+    echo "Auditing repository $repo ..."
+    # Set the repository owner and name
+    owner="SolidifyDemo"
+
+    # Get the list of workflow IDs
+    workflow_ids=$(gh api -H X-Github-Next-Global-ID:true /repos/$owner/$repo/actions/workflows | jq -r '.workflows[].id'  )
+
+    if [ -z "$workflow_ids" ]; then
+        echo "No workflows found for $repo"
+        continue
+    fi
+
+    # Loop over each workflow ID and get the latest run_started_at timestamp
+    runs=""
+    for id in $workflow_ids; do
+        response=$(gh api -H X-Github-Next-Global-ID:true repos/$owner/$repo/actions/workflows/$id/runs?per_page=1 | jq -r '{name: .workflow_runs[].name, last_run: .workflow_runs[].run_started_at?}' )
+        if [ -z "$response" ]; then
+            echo "No runs found for workflow $id"
+            continue
+        fi
+        runs="$runs$response,"
+    done
+
+    if [ -z "$runs" ]; then
+        echo "No runs found for $repo"
+        continue
+    fi
+
+    # Output the results as a JSON document
+    runs="${runs%?}"
+    echo "[$runs]" | REPO=$repo jq -r '[{name: env.REPO, workflows: .}]' > workflows_last_run.json
+    
+    cp actions_workflows_last_run.json tmp.json
+    jq -sc add workflows_last_run.json tmp.json > actions_workflows_last_run.json
+    rm -rf tmp.json workflows_last_run.json
+done <<< "$REPOS"
