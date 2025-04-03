e4e70da95bd6f97ac22664fb3a7fccdbadab7301
Ahmed Magdy <ahmed.magdy@solidify.dev>
2025-04-03 10:16:46 +0200

    Add a README.md in the misc-scripts folder and a comment at the top of the script as well

--- misc-scripts/custom-properties.sh
+++ misc-scripts/custom-properties.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script fetches repositories Name & isArchived a specified GitHub organization,
+# along with its custom properties, and saves the data to a CSV file.
+
 ORG="$1"
 LIMIT=${2:-1000}
 
