a09e84e5e1dc618d27ceaae39c3168400048ac48
Ahmed Magdy <ahmed.magdy@solidify.dev>
2025-05-07 10:53:00 +0200

    Update reapos-owners.sh to include owning teams as well

--- misc-scripts/repos-owners.sh
+++ misc-scripts/repos-owners.sh
@@ -11,28 +11,42 @@ FILE="${ORG}_repos_admins.csv"
 # List all repos in the org and sort them by name
 repos=$(gh repo list "$ORG" --json name,isArchived --limit 1000 | jq 'sort_by(.name | ascii_downcase)')
 
-echo "Repository, IsArchived, Admins" > "$FILE"
+echo "Repository, IsArchived, Admin users, Admin teams" > "$FILE"
 while IFS= read -r repo; do
-  echo "🔹 Repo: $repo"
-
   repo_name=$(echo "$repo" | jq -r '.name')
   is_archived=$(echo "$repo" | jq -r '.isArchived')
 
-  # Get ((Direct)) collaborators with admin access
+  # Get Direct collaborators with admin access
   logins=$(gh api "/repos/$ORG/$repo_name/collaborators?affiliation=direct&per_page=100" \
     --jq '.[] | select(.permissions.admin == true) | .login')
 
-  admins_list=""
+  # Get teams with admin access
+  team_logins=$(gh api "/repos/$ORG/$repo_name/teams?per_page=100" \
+    --jq '.[] | select(.permission == "admin") | .slug')
+
+  users_list=""
   for login in $logins; do
     # Fetch user's public name
     user_info=$(gh api "/users/$login")
     user_name=$(echo "$user_info" | jq -r '.name // "N/A"')
 
     # Append to the list of admins
-    admins_list+="$login ($user_name), "
+    users_list+="$login ($user_name), "
   done
   # Remove the trailing comma and space
-  admins_list=${admins_list%, }
+  users_list=${users_list%, }
+
+  teams_list=""
+  for team in $team_logins; do
+    # Fetch team's public name
+    team_info=$(gh api "/orgs/$ORG/teams/$team")
+    team_name=$(echo "$team_info" | jq -r '.name // "N/A"')
+
+    # Append to the list of teams
+    teams_list+="$team ($team_name), "
+  done
+
+  echo "🔹 Repo: $repo_name, Archived: $is_archived, Users: $users_list, Teams: $teams_list"
   # Write to the CSV file
-  echo "$repo_name, $is_archived, \"$admins_list\"" >> "$FILE"
+  echo "$repo_name, $is_archived, \"$users_list\", \"$teams_list\"" >> "$FILE"
 done <<< "$(echo "$repos" | jq -c '.[]')"
